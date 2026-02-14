7177791356e43cda994233ba914f2613b255705e
chanderlud <me@chanchan.dev>
2026-02-14 12:04:15 -0800

    switches to custom webpack build command approach

--- web/build_web.sh
+++ web/build_web.sh
@@ -1,1 +1,4 @@
-flutter_rust_bridge_codegen build-web --wasm-pack-rustflags "-Ctarget-feature=+atomics -Clink-args=--shared-memory -Clink-args=--max-memory=1073741824 -Clink-args=--import-memory -Clink-args=--export=__wasm_init_tls -Clink-args=--export=__tls_size -Clink-args=--export=__tls_align -Clink-args=--export=__tls_base" --rust-root rust/telepathy --release
+export RUSTUP_TOOLCHAIN=nightly
+export RUSTFLAGS="-Ctarget-feature=+atomics -Clink-args=--shared-memory -Clink-args=--max-memory=1073741824 -Clink-args=--import-memory -Clink-args=--export=__wasm_init_tls -Clink-args=--export=__tls_size -Clink-args=--export=__tls_align -Clink-args=--export=__tls_base"
+wasm-pack build -t no-modules -d /Users/chanchan/IdeaProjects/telepathy/web/pkg \
+--no-typescript --out-name telepathy rust/telepathy -- -Z build-std=std,panic_abort
