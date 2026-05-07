0bae67ba50c670f6553916bef860c7f5b8e8d650
chanderlud <me@chanchan.dev>
2026-05-06 17:39:40 -0700

    allows dead clode

--- rust/telepathy-core/src/overlay/error.rs
+++ rust/telepathy-core/src/overlay/error.rs
@@ -1,10 +1,12 @@
+#![allow(dead_code)]
+
 use std::fmt::Display;
 
 pub(crate) type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub(crate) struct Error {
-    _kind: ErrorKind,
+    kind: ErrorKind,
 }
 
 #[derive(Debug)]
@@ -17,7 +19,7 @@ enum ErrorKind {
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self._kind {
+        match &self.kind {
             #[cfg(windows)]
             ErrorKind::Windows(error) => write!(f, "windows error: {:?}", error),
             #[cfg(windows)]
@@ -31,7 +33,7 @@ impl Display for Error {
 impl From<widestring::error::ContainsNul<u16>> for Error {
     fn from(_: widestring::error::ContainsNul<u16>) -> Self {
         Error {
-            _kind: ErrorKind::ContainsNul,
+            kind: ErrorKind::ContainsNul,
         }
     }
 }
@@ -40,7 +42,7 @@ impl From<widestring::error::ContainsNul<u16>> for Error {
 impl From<windows::core::Error> for Error {
     fn from(error: windows::core::Error) -> Self {
         Error {
-            _kind: ErrorKind::Windows(error),
+            kind: ErrorKind::Windows(error),
         }
     }
 }
